59bec71f4ef891388fd9bca68e2611b8cbffe57d
Wouter <wouterfolkertsma@gmail.com>
2019-05-15 16:50:22 +0200

    Made the project error free

--- Contant.java
+++
@@ -1,8 +0,0 @@
-public class Contant extends Betaalwijze {
-    /**
-     * Methode om betaling af te handelen
-     */
-    public boolean betaal(double tebetalen) {
-        // method body omitted
-    }
-}
